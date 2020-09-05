fd84f5f53b9e0348591209a30141cdbf21272a17
sinha910diksha <sinha910diksha@gmail.com>
2020-09-05 23:54:50 +0530

    java_code

---
+++ learning_java/script.js
@@ -0,0 +1,39 @@
+const helloSpeaker = {
+	speakWord: 'Hello',	
+};
+//This says Hello
+(function () {
+	helloSpeaker.speak = function (userName) {
+		return (helloSpeaker.speakWord + " " + userName);
+	}
+}());
+
+
+//2
+/*Goodbye speaker function*/
+const byeSpeaker = {
+	speakWord: 'Good Bye'
+};
+
+(function () {
+	byeSpeaker.speak = function (userName) {
+		return (byeSpeaker.speakWord + " " + userName);
+	}
+}());
+
+//3
+//This choose the names
+(function () {
+	var names = ['Yaakov', 'John', 'Jen', 'Jason', 'Paul', 'Frank', 'Larry', 'Paula', 'Laura', 'Jim'];
+	for (var i = 0; i < names.length; i++) {
+		var name = names[i];
+		var firstLetter = name.charAt(0).toLowerCase();
+		if (firstLetter === 'j') {
+			console.log(byeSpeaker.speak(name));
+			document.write(byeSpeaker.speak(name) + '<br>');
+		} else {
+			console.log(helloSpeaker.speak(name));
+			document.write(helloSpeaker.speak(name + '<br>'));
+		}
+	}
+})();
